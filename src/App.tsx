d56339fae51d46a95385e6aa897753c3276a1dd2
arbi kustia <arbikustia14@gmail.com>
2023-03-24 14:23:12 +0700

    add landingPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './Pages/Home'
+import LandinPage from './Pages/LandinPage'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<LandinPage />} />
       </Routes>
     </BrowserRouter>
   )
