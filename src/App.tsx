e647de72d09c7b313c91ba603f0e6005351437e5
arbi kustia <arbikustia14@gmail.com>
2023-03-25 15:52:32 +0700

    add login and register

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
 import LandinPage from './Pages/LandinPage'
+import Login from './Pages/Login'
+import Register from './Pages/Register'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandinPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home/:id" element={<Home />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/Components/FormInput.tsx
@@ -0,0 +1,69 @@
+import React, { useState, ChangeEvent } from "react";
+import { AiOutlineEye } from "react-icons/ai";
+
+interface FormInputProps {
+    inputCount: number;
+    inputType: string[];
+    placeholder: string[];
+    onChange: (inputValues: string[]) => void;
+    width: string
+    mdWidth?: string
+    lgWidth?: string
+    xlWidth?: string
+    xxlWidth?: string
+}
+
+function FormInput({ inputCount, inputType, placeholder, onChange, width, mdWidth, lgWidth, xlWidth, xxlWidth }: FormInputProps) {
+    const [showPassword, setShowPassword] = useState<boolean>(false);
+
+    const handleTogglePassword = () => {
+        setShowPassword((prevState) => !prevState);
+    };
+
+    const [inputValues, setInputValues] = useState<string[]>(
+        new Array(inputCount).fill("")
+    );
+
+    const handleChange = (index: number, value: string) => {
+        setInputValues((prevState) => {
+            const newState = [...prevState];
+            newState[index] = value;
+            return newState;
+        });
+
+        onChange(inputValues);
+    };
+
+    const hidePw = () => {
+        return inputType.includes("text");
+    }
+
+    return (
+        <div>
+            {Array.from({ length: inputCount }, (_, i) => (
+                <div key={`input-${i}`} className="relative" >
+                    <input
+                        key={i}
+                        type={showPassword && inputType[i] === "password" ? "text" : inputType[i]}
+                        placeholder={placeholder[i]}
+                        className={`w-${width} md:w-${mdWidth} lg:w-${lgWidth} xl:w-${xlWidth} 2xl:w-${xxlWidth} px-2 py-1 border mt-5 border-black rounded-md focus:outline-none focus:ring focus:ring-white`}
+                        value={inputValues[i]}
+                        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                            handleChange(i, e.target.value)
+                        }
+                    />
+                    {
+                        inputType[i] === "password" && (
+                            <AiOutlineEye
+                                className="absolute right-2 top-7  cursor-pointer"
+                                onClick={handleTogglePassword}
+                            />
+                        )
+                    }
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default FormInput;
